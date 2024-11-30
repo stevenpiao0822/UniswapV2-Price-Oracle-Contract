f37b0af76522f54efd85a889e6afd4a5b99458f2
Steven Piao <stevenpiao0822@gmail.com>
2024-11-30 00:11:36 -0800

    [feat] defined the getTokenPrice func
    Co-Authored-By: Steven Leal <144204636+dodger213@users.noreply.github.com>

--- contracts/UniswapV2PriceOracle.sol
+++ contracts/UniswapV2PriceOracle.sol
@@ -38,7 +38,32 @@ contract UniswapV2PriceOracle {
         UNISWAP_V2_FACTORY_ADDRESS = _UNISWAP_V2_FACTORY_ADDRESS;
     }
 
-    function getPairAddress(address token) public view returns (address) {
-      return IUniswapV2Factory(UNISWAP_V2_FACTORY_ADDRESS).getPair(token, WETH);
+    function getPairAddress(
+        address tokenA,
+        address tokenB
+    ) public view returns (address) {
+        return
+            IUniswapV2Factory(UNISWAP_V2_FACTORY_ADDRESS).getPair(
+                tokenA,
+                tokenB
+            );
+    }
+
+    function getTokenPrice(
+        address token
+    )
+        public
+        view
+        returns (uint256 tokenPrice, uint256 ethPrice, string memory symbol)
+    {
+        address pairAddress = getPairAddress(token, WETH);
+        IUniswapV2Pair pairContract = IUniswapV2Pair(pairAddress);
+
+        (uint112 reserve0, uint112 reserve1, ) = pairContract.getReserves();
+        address token0 = pairContract.token0();
+        address token1 = pairContract.token1();
+
+        uint8 tokenDecimals = IERC20(token).decimals();
+        symbol = IERC20(token).symbol();
     }
 }
