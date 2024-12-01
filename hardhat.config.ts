c31056dd504f7037b9fbafb19480937c74cf028a
Steven Piao <stevenpiao0822@gmail.com>
2024-11-30 20:40:14 -0800

    [config] setup hardhat config
    Co-Authored-By: Steven Leal <144204636+dodger213@users.noreply.github.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,47 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const infuraKey: string = process.env.INFURA_API_KEY as string;
+const privateKey: string = process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY as string: "";
+const etherscanKey: string = process.env.ETHERSCAN_KEY ? process.env.ETHERSCAN_KEY as string: "";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 100,
+      },
+      // viaIR: true,
+    },
+  },
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${infuraKey}`,
+      accounts: [`0x${privateKey}`],
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${infuraKey}`,
+      accounts: [`0x${privateKey}`],
+    },
+    hardhat: {
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      mainnet: etherscanKey,
+      sepolia: etherscanKey
+    },
+  },
+  gasReporter: {
+    enabled: true,
+  },
+  sourcify: {
+    enabled: true,
+  },
 };
 
 export default config;
