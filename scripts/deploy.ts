ce4b0732344cb6531c5db6bbdb4b03597687e801
Steven Piao <stevenpiao0822@gmail.com>
2024-12-26 03:34:02 -0800

    [fix] fixed the deploy script
    Co-Authored-By: Steven Leal <144204636+dodger213@users.noreply.github.com>

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,7 +7,9 @@ async function main() {
   const weth = "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9";
   const uniswapv2factory = "0x7E0987E5b3a30e3f2828572Bb659A548460a3003";
   const uniswapV2PriceOracle = await ethers.deployContract("UniswapV2PriceOracle", [weth, uniswapv2factory]);
-  await uniswapV2PriceOracle.waitForDeployment()
+  await uniswapV2PriceOracle.waitForDeployment();
+  console.log(`Deploying contract with the account: ${deployer.address}`);
+  
   const UniswapV2PriceOracle_Address = await uniswapV2PriceOracle.getAddress();
   console.log(`UniswapV2PriceOracle is deployed. ${UniswapV2PriceOracle_Address}`);
 }
